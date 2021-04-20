b41d871153c0d62b36c5c2676db8f1179eb30eb1
Rencas1207 <rencasdag.12@gmail.com>
2021-04-19 23:57:45 -0500

    Added the type writing and line vertical

--- js/app.js
+++ js/app.js
@@ -61,6 +61,109 @@
 })(document);
 
 
+// Efecto Maquina escribir
+// const $text1 = document.querySelector('.text-title-1'),
+//     $text2 = document.querySelector('.text-title-2'),
+//     $text3 = document.querySelector('.text-title-3'),
+//     $string1 = $text1.innerHTML;
+//     $string2 = $text2.innerHTML;
+//     $string3 = $text3.innerHTML;
+
+// $text1.innerHTML = "";
+// $text2.innerHTML = "";
+// $text3.innerHTML = "";
+
+// let speed1 = 150,
+// speed2 = 150,
+// speed3 = 150,
+//     i = 0,
+//     j = 0,
+//     k = 0;
+
+// function effectWrite1(){
+//     if(i < $string1.length){
+//         $text1.innerHTML += $string1.charAt(i);
+//         i++;
+//         setTimeout(effectWrite1, speed1);
+//     }
+
+// }
+
+// function effectWrite2(){
+//     if(j < $string2.length){
+//         $text2.innerHTML += $string2.charAt(j);
+//         j++;
+//         setTimeout(effectWrite2, speed2);
+//     }
+// }
+
+// function effectWrite3(){
+//     if(k < $string3.length){
+//         $text3.innerHTML += $string3.charAt(k);
+//         k++;
+//         setTimeout(effectWrite3, speed3);
+//     }
+// }
+
+// setTimeout(effectWrite1, speed1);
+// setTimeout(effectWrite2, speed2);
+// setTimeout(effectWrite3, speed3);
+
+// const $text1 = document.querySelector('.text-title-1'),
+//     $text2 = document.querySelector('.text-title-2'),
+//     $text3 = document.querySelector('.text-title-3');
+//     $string1 = $text1.innerHTML;
+//     $string2 = $text2.innerHTML;
+//     $string3 = $text3.innerHTML;
+
+
+const $text1 = document.querySelector('.text-title-1'),
+    $text2 = document.querySelector('.text-title-2'),
+    $text3 = document.querySelector('.text-title-3');
+
+let writing = (string1 , string2, string3) => {
+    $text1.innerHTML = "";
+    $text2.innerHTML = "";
+    $text3.innerHTML = "";
+    let arrFromString = string1.split('');
+    let i = 0;
+    let printString1 = setInterval(() => {
+        $text1.innerHTML += arrFromString[i];
+        i++;
+
+        if(i === arrFromString.length){
+            clearInterval(printString1);
+
+            let arrFromString2 = string2.split('');
+            i = 0;
+            let printString2 = setInterval(() => {
+                $text2.innerHTML += arrFromString2[i];
+                i++;
+
+                if(i === arrFromString2.length){
+                    clearInterval(printString2);
+                    
+                    let arrFromString3 = string3.split('');
+                    i = 0;
+                    let printString3 = setInterval(() => {
+                        $text3.innerHTML += arrFromString3[i];
+                        i++;
+
+                        if(i === arrFromString3.length){
+                            clearInterval(printString3);
+                        }
+                    }, 80);
+                }
+            }, 80);
+            // document.body.style.color = 'steelblue';
+        }
+    }, 80);
+};  
+
+writing($text1.textContent, $text2.textContent, $text3.textContent);
+
+
+
 // const $hamburgesa = document.querySelector("#container-menu"),
 //     $cerrar = document.querySelector(".icon-close"),
 //     $menu = document.querySelector(".menu");
