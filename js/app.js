0d51a0135e279aaee601357039cb50af79f1d8e8
Rencas1207 <rencasdag.12@gmail.com>
2021-04-21 15:01:53 -0500

    Cambios

--- js/app.js
+++ js/app.js
@@ -1,42 +1,8 @@
-// ((d) => {
-//     const $btnMenu = d.querySelector(".menu-btn"),
-//         $navegacion = d.querySelector(".navegacion");
-    
-//         $btnMenu.addEventListener("click", e => {
-//             $btnMenu.firstElementChild.classList.toggle("none");
-//             $btnMenu.lastElementChild.classList.toggle("none");
-//             $navegacion.classList.toggle("is-active");
-//         });
+document.addEventListener("DOMContentLoaded", e => {
+    scrollTopButton(".scroll-top-btn");
+    navegacionFija();
+});
 
-//         d.addEventListener("click", e => {
-//             if(!e.target.matches(".navegacion a")) return false;
-
-//             $btnMenu.firstElementChild.classList.remove("none");
-//             $btnMenu.lastElementChild.classList.add("none");
-//             $navegacion.classList.remove("is-active");
-//         });
-// })(document);
-
-// console.log('Hola mundo');
-
-// let botonMenu = document.querySelector(".icon-menu"),
-//     botonClose = document.querySelector('.icon-close'),
-//     navegacion = document.querySelector(".enlaces");
-
-// document.addEventListener('click', e => {
-//     botonMenu.classList.toggle('none');
-//     botonClose.classList.toggle('none');
-//     // navegacion.classList.toggle('dos');
-//     navegacion.classList.toggle('is-active');
-// })
-
-// document.addEventListener("click",(e) => {
-//     if(!e.target.matches(".enlaces a")) return false;
-
-//     // $btnMenu.firstElementChild.classList.remove("none");
-//     // $btnMenu.lastElementChild.classList.add("none");
-//     // $menu.classList.remove("is-active");
-// });
 
 ((d) => {
     const $btnMenu = d.querySelector(".icon"),
@@ -59,63 +25,6 @@
 
         
 })(document);
-
-
-// Efecto Maquina escribir
-// const $text1 = document.querySelector('.text-title-1'),
-//     $text2 = document.querySelector('.text-title-2'),
-//     $text3 = document.querySelector('.text-title-3'),
-//     $string1 = $text1.innerHTML;
-//     $string2 = $text2.innerHTML;
-//     $string3 = $text3.innerHTML;
-
-// $text1.innerHTML = "";
-// $text2.innerHTML = "";
-// $text3.innerHTML = "";
-
-// let speed1 = 150,
-// speed2 = 150,
-// speed3 = 150,
-//     i = 0,
-//     j = 0,
-//     k = 0;
-
-// function effectWrite1(){
-//     if(i < $string1.length){
-//         $text1.innerHTML += $string1.charAt(i);
-//         i++;
-//         setTimeout(effectWrite1, speed1);
-//     }
-
-// }
-
-// function effectWrite2(){
-//     if(j < $string2.length){
-//         $text2.innerHTML += $string2.charAt(j);
-//         j++;
-//         setTimeout(effectWrite2, speed2);
-//     }
-// }
-
-// function effectWrite3(){
-//     if(k < $string3.length){
-//         $text3.innerHTML += $string3.charAt(k);
-//         k++;
-//         setTimeout(effectWrite3, speed3);
-//     }
-// }
-
-// setTimeout(effectWrite1, speed1);
-// setTimeout(effectWrite2, speed2);
-// setTimeout(effectWrite3, speed3);
-
-// const $text1 = document.querySelector('.text-title-1'),
-//     $text2 = document.querySelector('.text-title-2'),
-//     $text3 = document.querySelector('.text-title-3');
-//     $string1 = $text1.innerHTML;
-//     $string2 = $text2.innerHTML;
-//     $string3 = $text3.innerHTML;
-
 
 const $text1 = document.querySelector('.text-title-1'),
     $text2 = document.querySelector('.text-title-2'),
@@ -162,7 +71,30 @@ let writing = (string1 , string2, string3) => {
 
 writing($text1.textContent, $text2.textContent, $text3.textContent);
 
+function scrollTopButton(btn){
+    const $scrollBtn = document.querySelector(btn);
 
+    window.addEventListener('scroll', e => {
+        //Retorna el numero de pixeles que han sido desplazado en el documento mediante el scroll vertical 
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if(scrollTop > 600){
+            $scrollBtn.classList.remove("hidden");
+        }else{
+            $scrollBtn.classList.add("hidden");
+        }
+    })
+
+    document.addEventListener('click', e => {
+        if(e.target.matches(btn)){
+
+            window.scrollTo({
+                behavior: "smooth",
+                top: 0
+            })
+        }
+    });
+}
 
 // const $hamburgesa = document.querySelector("#container-menu"),
 //     $cerrar = document.querySelector(".icon-close"),
@@ -182,3 +114,30 @@ writing($text1.textContent, $text2.textContent, $text3.textContent);
 //     $menu.style.opacity = "1";
 //     $menu.style.visibility = "none";
 // }
+
+
+// Efecto Maquina escribir
+// const $text1 = document.querySelector('.text-title-1'),
+//     $text2 = document.querySelector('.text-title-2'),
+//     $text3 = document.querySelector('.text-title-3'),
+//     $string1 = $text1.innerHTML;
+//     $string2 = $text2.innerHTML;
+//     $string3 = $text3.innerHTML;
+
+// $text1.innerHTML = "";
+// $text2.innerHTML = "";
+// $text3.innerHTML = "";
+
+// let speed1 = 150,
+// speed2 = 150,
+// speed3 = 150,
+//     i = 0,
+//     j = 0,
+//     k = 0;
+
+// function effectWrite1(){
+//     if(i < $string1.length){
+//         $text1.innerHTML += $string1.charAt(i);
+//         i++;
+//         setTimeout(effectWrite1, speed1);
+//     }
