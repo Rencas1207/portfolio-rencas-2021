b8a9b44b58b2730758e1b3e86b11dcca1bb7e1b5
Rencas1207 <rencasdag.12@gmail.com>
2021-04-26 22:14:33 -0500

    Agregando cambios 26/04/2021

--- js/app.js
+++ js/app.js
@@ -129,6 +129,40 @@ function showScroll(){
 
 window.addEventListener('scroll', showScroll);
 
+/*---------ContactForm--------------*/
+((d) => {
+    const $form = d.querySelector(".formulario"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-responsive");
+
+    $form.addEventListener("submit",e => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/rencasdag.12@gmail.com",{
+            method: "POST",
+            body: new FormData(e.target),
+        })
+        .then((res) => (res.ok ? res.json(): Promise.reject(res)))
+        .then((json) =>{
+            console.log(json);
+            $loader.classList.add("none");
+            location.hash = "#gracias";
+            $form.reset();
+        })
+        .catch((err) =>{
+            console.log(err);
+            let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente"
+            $response.querySelector("h3").innerHTML = `Error ${err.status}:${message}`;
+            $loader.classList.add("none");
+        }).finally(()=> {
+            $loader.classList.add("none");
+            setTimeout(() => {
+                location.hash = "#close";
+            }, 3000);
+        });
+    });
+})(document);
+
 // const $hamburgesa = document.querySelector("#container-menu"),
 //     $cerrar = document.querySelector(".icon-close"),
 //     $menu = document.querySelector(".menu");
